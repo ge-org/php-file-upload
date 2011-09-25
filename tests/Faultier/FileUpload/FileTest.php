4df7bff5891ebe48544052f92ef0746d45461e38
Georg Dresler <georg@g-dresler.de>
2011-09-25 14:44:39 +0200

    blub3

--- tests/Faultier/FileUpload/FileTest.php
+++ tests/Faultier/FileUpload/FileTest.php
@@ -1,17 +1,16 @@
 <?php
 
-	require_once __DIR__.'/../../../lib/Faultier/FileUpload/File.php';
-
 	class FileTest extends PHPUnit_Framework_TestCase {
 	
 		protected $file;
 		
 		protected function setUp() {
-			$this->file = new File();
+			$this->file = null;//new File();
 		}
 		
 		public function testFileCreated() {
-			$this->assertInstanceOf('File', $this->file);
+			$this->assertTrue(false);
+			//$this->assertInstanceOf('File', $this->file);
 		}
 	
 	}
