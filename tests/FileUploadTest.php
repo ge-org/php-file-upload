bac35e943830e6fa91d3622d01beb1a969e65b1d
Georg Dresler <georg@g-dresler.de>
2011-10-01 12:13:06 +0200

    foo

--- tests/FileUploadTest.php
+++ tests/FileUploadTest.php
@@ -4,46 +4,11 @@
 
 	class FileUploadTest extends \PHPUnit_Framework_TestCase {
 	
-		protected $testDirectory;
-		protected $up;
+		// TODO use vfsStream
+		// TODO write tests
 		
-		public function setUp() {
-			$this->testDirectory = __DIR__.'/test-files';
-			@mkdir($this->testDirectory);
-			
-			$this->up = new FileUpload($this->testDirectory);
-		}
-		
-		public function tearDown() {
-			@rmdir($this->testDirectory);
-		}
-		
-		public function testInstantiationWithValidUploadDirectory() {
-			$this->assertInstanceOf('Faultier\FileUpload\FileUpload', $this->up);
-		}
-		
-		public function testUploadDirectoryNull() {
-			$this->setExpectedException('\InvalidArgumentException', 'The given upload directory does not exist');
-			$this->up->setUploadDirectory(null);
-		}
-		
-		public function testUploadDirectoryWithEmptyString() {
-			$this->setExpectedException('\InvalidArgumentException', 'The given upload directory does not exist');
-			$this->up->setUploadDirectory('');
-		}
-		
-		public function testUploadDirectoryWithFile() {
-			$this->setExpectedException('\InvalidArgumentException', 'The given upload directory is not a directory');
-			$this->up->setUploadDirectory(__DIR__.'/FileUploadTest.php');
-		}
-		
-		public function testUploadDirectoryWithUnwritable() {
-			// TODO
-		}
-		
-		public function testUploadDirectory() {
-			$this->up->setUploadDirectory($this->testDirectory);
-			$this->assertEquals($this->testDirectory, $this->up->getUploadDirectory());
+		public function testHelloWorld() {
+			$this->markTestIncomplete('This test has not been implemented yet.');
 		}
 		
 	}
