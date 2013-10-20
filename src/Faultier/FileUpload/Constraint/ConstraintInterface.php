ffc2acf96eac120e7a5e972e3ad0238a44450ff8
Ujjwal Ojha <bring2uo@gmail.com>
2013-10-19 23:23:34 -0700

    Removed SetOptions And Added parse

--- src/Faultier/FileUpload/Constraint/ConstraintInterface.php
+++ src/Faultier/FileUpload/Constraint/ConstraintInterface.php
@@ -6,7 +6,7 @@ use Faultier\FileUpload\File;
 	
 interface ConstraintInterface {
 	
-	public function setOptions($options);
+	public function parse($options);
 	public function isValid(File $file);
 
     public function getErrors();
